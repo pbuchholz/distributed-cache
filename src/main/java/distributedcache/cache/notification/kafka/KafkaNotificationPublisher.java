fc4331b151d94ee905f78d5862b0a8cf992472fc
Philipp Buchholz <philipp.buchholz@gmx.net>
2020-05-26 09:50:12 +0200

    Cleaned up  sending of Notifications.

--- src/main/java/distributedcache/cache/notification/kafka/KafkaNotificationPublisher.java
+++ src/main/java/distributedcache/cache/notification/kafka/KafkaNotificationPublisher.java
@@ -1,11 +1,13 @@
 package distributedcache.cache.notification.kafka;
 
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 
 import javax.enterprise.context.Dependent;
 
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
 
 import distributedcache.cache.notification.Notification;
 import distributedcache.cache.notification.NotificationPublisher;
@@ -25,21 +27,26 @@ public class KafkaNotificationPublisher<K, T>
 
 	@Override
 	public void publish(KafkaSubscription<K, Notification<T>> subscription, Notification<T> notification) {
-//		try {
+		try {
 			/* Dont publish in case its an in only configuration. */
 			if (!subscription.outTopic().isPresent()) {
 				return;
 			}
 
 			Producer<K, Notification<T>> notificationProducer = subscription.getProducer();
-			//notificationProducer.send(new ProducerRecord<>(subscription.outTopic().getTopicName(), notification)).get();
 
-			notificationProducer.send(new ProducerRecord<>(subscription.outTopic().getTopicName(), notification),
-					(metadata, exception) -> {
-						// Handle returned metadata and exception here
-					});
-//		} catch (InterruptedException | ExecutionException e) {
-//			throw new RuntimeException(e);
-//		}
+			Future<RecordMetadata> sendFuture = notificationProducer
+					.send(new ProducerRecord<>(subscription.outTopic().getTopicName(), notification));
+			
+			/* Wait till result is received. */
+			sendFuture.get();
+			if (!sendFuture.isDone()) {
+				throw new RuntimeException(
+						String.format("Notification %1 could not be send.", notification.toString()));
+
+			}
+		} catch (InterruptedException | ExecutionException e) {
+			throw new RuntimeException(e);
+		}
 	}
 }
